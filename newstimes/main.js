002799488c7698d5a707d4f25ac4d28b22daf91c
이지영 <110140031+bgbg5107@users.noreply.github.com>
2022-10-24 11:45:15 +0900

    js 수정
    api 호출 에러 해결하기

--- newstimes/main.js
+++ newstimes/main.js
@@ -10,14 +10,51 @@ const getLastestNews = async () => {
   let data = await response.json();
   news = data.articles;
   console.log(news);
+  render();
 };
+
+const render=()=>{
+  let newsHTML = "";
+  newsHTML = news.map((news) =>{
+    return `<div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="news-img-size"
+        src="https://static01.nyt.com/images/2022/10/07/books/00MATTHEWPERRY1/00MATTHEWPERRY1-threeByTwoMediumAt2X.jpg?format=pjpg&quality=75&auto=webp&disable=upscale"
+      />
+    </div>
+    <div class="col-lg-8">
+      <h2 class="news-title">Title</h2>
+      <p>Contents</p>
+      <div>SBS * 2022-10-24</div>
+    </div>
+  </div>`
+  });
+  console.log(newsHTML)
+  document.getElementById("news-board").innerHTML = newsHTML;
+}
 getLastestNews();
 
+
+let menu = document.getElementById("menu-bar");
 function openMenu() {
-  let menu = document.getElementById("menu-bar");
   if (menu.style.display == "none") {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
+  }
+}
+
+let closeMenu = document.querySelector('.menu-icon');
+closeMenu.addEventListener("mouseout",()=>{
+  menu.style.display = "none";
+});
+
+let searchBox = document.getElementById("searchBox")
+function openSearch(){
+  if (searchBox.style.display == "none") {
+    searchBox.style.display = "block";
+  } else {
+    searchBox.style.display = "none";
   }
 }
