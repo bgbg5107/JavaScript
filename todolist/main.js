647040b754e6a62b50faefdae343ef56ac59ba22
이지영 <110140031+bgbg5107@users.noreply.github.com>
2022-10-26 15:30:02 +0900

    마무리

--- todolist/main.js
+++ todolist/main.js
@@ -1,35 +1,8 @@
-//유저가 값을 입력한다.
-// + 버튼을 클릭하면, 할일이 추가된다.
-//delete 버튼을 클릭하면 할일이 삭제된다.
-//check 버튼을 클릭하면 할일이 끝나면서 밑줄이 쳐진다.
-//1. check 버튼을 클릭하는 순간 true false
-//2. true이면 끝난 것으로 간주하고 밑줄 보여주기
-//3. false이면 끝나지 않은 것으로 간주하고 그대로 보여주기
-
-//진행중 끝남 탭을 누르면, 언더바가 이동한다
-//끝남탭은 끝난 아이템만, 진행중 탭은 진행중인 아이템만
-//전체탭을 누르면 다시 전체아이템으로 돌아온다.
-
 let taskInput = document.getElementById("task-input");
-let addButton = document.getElementById("add-button");
 let tabs = document.querySelectorAll(".task-tabs div"); //여러개 선택
 let taskList = [];
 let filterList = [];
 let mode = "all";
-
-addButton.addEventListener("click", addTask=()=>{
-  let task = {
-    id: randomIDGenerate(),
-    taskContent: taskInput.value,
-    isComplete: false,
-  };
-  taskList.push(task);
-  if(task.taskContent == ""){
-    taskList.pop(task)
-  }
-  render();
-  console.log(taskList)
-});
 
 taskInput.addEventListener("focus", focusInput=()=>{
   taskInput.value = "";
@@ -42,23 +15,30 @@ taskInput.addEventListener("keypress", (event) =>{
   }
 });
 
+const addTask=()=>{
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  if(task.taskContent == "") taskList.pop(task)
+  render();
+};
+
 for (let i = 1; i < tabs.length; i++) {
   tabs[i].addEventListener("click", (event)=> {
     filter(event);
   });
 }
 
-
  const render=()=>{
   let list = [];
   let resultHTML = "";
 
-  if (mode == "all") {
-    list = taskList;
-  } else {
-    list = filterList;
-  }
-
+  if (mode == "all") list = taskList;
+  else list = filterList;
+  
   for (let i = 0; i < list.length; i++) {
     if (list[i].isComplete == true) {
       resultHTML += `<div class="task">
@@ -92,11 +72,8 @@ const toggleComplete=(id)=> {
 }
 
 const deleteTask=(id)=> {
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
-      taskList.splice(i, 1);
-    }
-  }
+  for (let i = 0; i < taskList.length; i++) 
+    if (taskList[i].id == id) taskList.splice(i, 1);
   filter();
 }
 
@@ -113,15 +90,11 @@ const filter=(event)=> {
   filterList = [];
   if (mode == "ongoing") {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete == false) {
-        filterList.push(taskList[i]);
-      }
+      if (taskList[i].isComplete == false) filterList.push(taskList[i]);
     }
   } else if (mode == "done") {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete == true) {
-        filterList.push(taskList[i]);
-      }
+      if (taskList[i].isComplete == true) filterList.push(taskList[i]);
     }
   }
   render();
@@ -130,6 +103,5 @@ const filter=(event)=> {
 const randomIDGenerate=()=> {
   return (performance.now().toString(36) + Math.random().toString(36)).replace(
     /\./g,
-    ""
-  );
+    "");
 }
